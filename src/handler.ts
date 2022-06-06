80b90e00ddcfe8d9f94653f3465a3e8b9063e97d
AirFusion45 <jfang.cv.ca.us@gmail.com>
2022-06-05 20:57:51 -0700

    working demo code

--- src/handler.ts
+++ src/handler.ts
@@ -1,15 +1,13 @@
-import { Router } from 'itty-router';
+import { Router } from 'itty-router'
 
-// import Posts from './handlers/posts';
-// import Post from './handlers/post';
-import Paper from './handlers/paper';
+import Posts from './handlers/posts'
+import Post from './handlers/post'
 
-const router = Router();
+const router = Router()
 
 router
-  // .get('/api/posts', Posts)
-  // .get('/api/posts/:id', Post)
-  .get('/api/paper', Paper)
-  .get('*', () => new Response('Not found', { status: 404 }));
+  .get('/api/posts', Posts)
+  .get('/api/posts/:id', Post)
+  .get('*', () => new Response("Not found", { status: 404 }))
 
-export const handleRequest = request => router.handle(request);
+export const handleRequest = request => router.handle(request)
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 const path = require('path')
+const webpack = require('webpack')
+
+const mode = process.env.NODE_ENV || 'production'
 
 module.exports = {
-  entry: './src/index.ts',
   output: {
-    filename: 'worker.js',
+    filename: `worker.${mode}.js`,
     path: path.join(__dirname, 'dist'),
   },
-  devtool: 'cheap-module-source-map',
-  mode: 'development',
+  mode,
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
+    plugins: [],
   },
   module: {
     rules: [
@@ -17,8 +19,7 @@ module.exports = {
         test: /\.tsx?$/,
         loader: 'ts-loader',
         options: {
-          // transpileOnly is useful to skip typescript checks occasionally:
-          // transpileOnly: true,
+          transpileOnly: true,
         },
       },
     ],
