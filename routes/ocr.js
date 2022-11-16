b8d21dec6043e0bfa49ec659c43b5ed670632506
AirFusion45 <jfang.cv.ca.us@gmail.com>
2022-11-15 22:09:40 -0800

    ocr works - need to postback & setup dockers, etc. or start on get

--- routes/ocr.js
+++ routes/ocr.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { createWorker, createScheduler } = require('tesseract.js')
-const os = require('os')
-// const queue = require('Bull')
-const scheduler = createScheduler()
+const { createWorker } = require('tesseract.js')
 
 router.post('/', async (req, resApp) => {
-    if (req.body.auth === process.env.AUTH) {
+    if (req.body.auth !== process.env.AUTH) {
         return resApp.status(401).send('Invalid API Key or no authentication provided.')
     }
     // Input Data Schema
@@ -18,53 +15,49 @@ router.post('/', async (req, resApp) => {
     //     lang: <String>
     //     source: <URL>
     // }
-
-    // respond to request
-
-    // if english - multiworker suppport
-    // other lang - single threaded job
-    if (lang === 'eng') {
-        // check if there are workers created: if yes: add to queue, if no: create workers and add to queue
-        if (scheduler.getNumWorkers() === 0) { // no workers - create new
-            let tempWorker
-            for (i = 0; i < os.cpus().length; i++) {
-                tempWorker = createWorker()
-                await tempWorker.load()
-                await worker.loadLanguage('eng')
-                await worker.initialize('eng')
-                scheduler.addWorker(tempWorker)
-            }
-        }
-        // add to queue
-        const { data } = await scheduler.addJob('detect', image)
-    } else {
-
+    let redisDataInput = {
+        serverID: req.body.serverID,
+        channelID: req.body.channelID,
+        memberID: req.body.memberID,
+        jobID: req.body.jobID,
+        lang: req.body.lang,
+        source: req.body.source
     }
+    // respond to init request 200 ok
+    resApp.status(200).send('OK')
 
-    // await for complete
+    // start OCR! - add to ocr job
+    console.time()
+    ocr(redisDataInput, req)
 
-    // post back
-
-    const manualQueue = req.app.get('manualQueue')
-    manualQueue.connect()
-    await manualQueue.set(res.data.jobID, JSON.stringify(res.data), { // params: (key, value)
-        EX: 86400 // 1 day
-    })
-
+    // always keep one english worker idling
+    // add new workers as needed; but less than core count. if not english, dont use queue
+    // remove workers when not needed
 })
 
+async function ocr(param, req) {
 
-// async function ocr(metaInfo, resApp) {
-//     const worker = createWorker()
-//     await worker.load()
-//     await worker.loadLanguage(metaInfo.lang)
-//     await worker.initialize(metaInfo.lang)
-//     const { data: { text } } = await worker.recognize(metaInfo.source)
-//     metaInfo['result'] = Buffer.from(text)
-//     await worker.terminate()
-// }
+    // use existing workers if lang is english else create new worker without queue
+    if (param.lang === 'eng') {
 
+        const tesseractScheduler = req.app.get('tesseractScheduler')
 
-
+        var data = await tesseractScheduler.addJob('recognize', param.source, {}, param.jobID)
+    
+        // append to params + postback to /ocr
+    } else {
+        const worker = createWorker()
+        await worker.load()
+        await worker.loadLanguage(param.lang)
+        await worker.initialize(param.lang)
+        var data = await worker.recognize(param.source, param.lang, param.jobID)
+        worker.terminate()
+        // append to params + postback to /ocr
+    }
+    // send request
+    console.log(data.jobId)
+    console.log(data.data.text)
+    console.timeEnd()
+}
 
 module.exports = router
