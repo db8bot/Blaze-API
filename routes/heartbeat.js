36e274e6317315be72e4196232f85cdf5fe7d612
AirFusion45 <jfang.cv.ca.us@gmail.com>
2023-02-24 21:14:31 -0800

    update heartbeat

--- routes/heartbeat.js
+++ routes/heartbeat.js
@@ -2,11 +2,14 @@ const express = require('express')
 const router = express.Router()
 const os = require('os')
 const process = require('process')
+const package = require('../package.json')
 
 router.get('/', (req, resApp) => {
     // no api key needed
     let response = {
         status: true,
+        version: package.version,
+        processUptime: process.uptime(),
         // server information
         arch: os.arch(),
         platform: os.platform(),
